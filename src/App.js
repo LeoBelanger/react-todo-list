78a593ea4b2aec2c7b4f46bc68a294b14f714592
Leo Belanger <leobelanger1996@gmail.com>
2020-08-12 14:16:12 -0700

    added about page, added router and links to transition between pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Header from "./components/layout/Header";
 import Todos from "./components/Todos";
 import AddTodo from "./components/AddTodo";
+import About from "./components/pages/About";
 import "./App.css";
 import { v4 as uuidv4 } from "uuid";
 
@@ -16,7 +18,7 @@ class App extends Component {
       {
         id: uuidv4(),
         title: "Dinner",
-        completed: true,
+        completed: false,
       },
       {
         id: uuidv4(),
@@ -58,17 +60,28 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <div className="container">
-          <Header />
-          <AddTodo addTodo={this.addTodo} />
-          <Todos
-            todos={this.state.todos}
-            markComplete={this.markComplete}
-            deleteTodo={this.deleteTodo}
-          />
+      <Router>
+        <div className="App">
+          <div className="container">
+            <Header />
+            <Route
+              exact
+              path="/"
+              render={(props) => (
+                <React.Fragment>
+                  <AddTodo addTodo={this.addTodo} />
+                  <Todos
+                    todos={this.state.todos}
+                    markComplete={this.markComplete}
+                    deleteTodo={this.deleteTodo}
+                  />
+                </React.Fragment>
+              )}
+            />
+            <Route path="/about" component={About} />
+          </div>
         </div>
-      </div>
+      </Router>
     );
   }
 }
