5f2cf52b87bbbaeb93fb1aa7e590d2189354cfed
Leo Belanger <leobelanger1996@gmail.com>
2020-08-12 12:37:46 -0700

    added prettier extension for consistent code quality, added addTodo component, added header component

--- src/App.js
+++ src/App.js
@@ -1,49 +1,60 @@
-import React, { Component } from 'react';
-import Todos from './components/Todos';
-import './App.css';
+import React, { Component } from "react";
+import Header from "./components/layout/Header";
+import Todos from "./components/Todos";
+import AddTodo from "./components/AddTodo";
+import "./App.css";
 
 class App extends Component {
   state = {
     todos: [
       {
         id: 1,
-        title: 'Take out the trash',
-        completed: false
+        title: "Take out the trash",
+        completed: false,
       },
       {
         id: 2,
-        title: 'Dinner',
-        completed: true
+        title: "Dinner",
+        completed: true,
       },
       {
         id: 3,
-        title: 'Gym',
-        completed: false
-      }
-    ]
-  }
+        title: "Gym",
+        completed: false,
+      },
+    ],
+  };
 
   // Toggle complete
   markComplete = (id) => {
-    this.setState({ todos: this.state.todos.map(todo => {
-      if(todo.id === id) {
-        todo.completed = !todo.completed; // set it to other value
-      }
-      return todo;
-    }) });
-  }
+    this.setState({
+      todos: this.state.todos.map((todo) => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed; // set it to other value
+        }
+
+        return todo;
+      }),
+    });
+  };
 
   // Delete todo
   deleteTodo = (id) => {
-    
-  }
+    this.setState({
+      todos: [...this.state.todos.filter((todo) => todo.id !== id)],
+    });
+  };
 
   render() {
     return (
       <div className="App">
-        <Todos todos={this.state.todos}
-               markComplete={this.markComplete} 
-               deleteTodo={this.deleteTodo} /> 
+        <Header />
+        <AddTodo />
+        <Todos
+          todos={this.state.todos}
+          markComplete={this.markComplete}
+          deleteTodo={this.deleteTodo}
+        />
       </div>
     );
   }
